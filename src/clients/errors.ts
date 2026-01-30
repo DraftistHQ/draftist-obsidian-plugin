40ef3912867ddddaf8ec1ed866ed190b5f85e414
Alex <alex@35mil.me>
2026-01-30 14:21:28 +0300

    refactor(obsidian): extract shared content error schemas to errors module

---
+++ src/clients/errors.ts
@@ -0,0 +1,130 @@
+import { z } from "zod"
+
+export const ContentParsingError = z.discriminatedUnion("TAG", [
+    z.object({
+        TAG: z.literal("UnimplementedNode"),
+        node: z.string(),
+    }),
+    z.object({
+        TAG: z.literal("UnexpectedNode"),
+        node: z.string(),
+    }),
+    z.object({
+        TAG: z.literal("HeadingNodeConversionError"),
+        error: z.object({ TAG: z.literal("TooDeep"), level: z.number() }),
+    }),
+    z.object({
+        TAG: z.literal("ListNodeConversionError"),
+        error: z.union([
+            z.object({ TAG: z.literal("UnimplementedNode"), node: z.string() }),
+            z.object({ TAG: z.literal("UnexpectedNode"), node: z.string() }),
+            z.object({
+                TAG: z.literal("SubListConversionError"),
+                error: z.array(
+                    z.union([
+                        z.object({ TAG: z.literal("UnimplementedNode"), node: z.string() }),
+                        z.object({ TAG: z.literal("UnexpectedNode"), node: z.string() }),
+                        z.object({ TAG: z.literal("SubListConversionError"), error: z.any() }), // Recursive
+                    ]),
+                ),
+            }),
+        ]),
+    }),
+    z.object({
+        TAG: z.literal("BlockquoteNodeConversionError"),
+        error: z.discriminatedUnion("TAG", [
+            z.object({ TAG: z.literal("UnimplementedNode"), node: z.string() }),
+            z.object({ TAG: z.literal("UnexpectedNode"), node: z.string() }),
+        ]),
+    }),
+    z.object({
+        TAG: z.literal("CalloutError"),
+        error: z.union([
+            z.literal("InvalidSyntax"),
+            z.object({ TAG: z.literal("UnexpectedCalloutVariant"), variant: z.string() }),
+            z.object({
+                TAG: z.literal("BlockParsingError"),
+                error: z.discriminatedUnion("TAG", [
+                    z.object({ TAG: z.literal("UnsupportedNode"), node: z.string() }),
+                    z.object({
+                        TAG: z.literal("HeadingError"),
+                        error: z.object({ TAG: z.literal("TooDeep"), level: z.number() }),
+                    }),
+                    z.object({
+                        TAG: z.literal("ListError"),
+                        error: z.union([
+                            z.object({ TAG: z.literal("UnimplementedNode"), node: z.string() }),
+                            z.object({ TAG: z.literal("UnexpectedNode"), node: z.string() }),
+                            z.object({ TAG: z.literal("SubListConversionError"), error: z.any() }), // Recursive
+                        ]),
+                    }),
+                    z.object({
+                        TAG: z.literal("QuoteError"),
+                        error: z.discriminatedUnion("TAG", [
+                            z.object({ TAG: z.literal("UnimplementedNode"), node: z.string() }),
+                            z.object({ TAG: z.literal("UnexpectedNode"), node: z.string() }),
+                        ]),
+                    }),
+                    z.object({
+                        TAG: z.literal("ImageError"),
+                        error: z.discriminatedUnion("TAG", [
+                            z.object({ TAG: z.literal("UnexpectedParams"), params: z.string() }),
+                            z.object({ TAG: z.literal("UnexpectedPlacementValue"), value: z.string() }),
+                            z.object({ TAG: z.literal("ImageNotFound"), url: z.string() }),
+                        ]),
+                    }),
+                    z.object({
+                        TAG: z.literal("VideoError"),
+                        error: z.union([
+                            z.object({ TAG: z.literal("InvalidYouTubeUrl"), url: z.string() }),
+                            z.object({ TAG: z.literal("UnexpectedYouTubeUrl"), url: z.string() }),
+                            z.literal("YouTubeUrlContainsInvalidChars"),
+                        ]),
+                    }),
+                    z.object({
+                        TAG: z.literal("GalleryError"),
+                        error: z.union([
+                            z.object({ TAG: z.literal("UnexpectedNode"), node: z.string() }),
+                            z.object({ TAG: z.literal("MissingImage"), url: z.string() }),
+                            z.literal("EmptyGallery"),
+                        ]),
+                    }),
+                ]),
+            }),
+        ]),
+    }),
+    z.object({
+        TAG: z.literal("ImageConversionError"),
+        error: z.discriminatedUnion("TAG", [
+            z.object({ TAG: z.literal("UnexpectedParams"), params: z.string() }),
+            z.object({ TAG: z.literal("UnexpectedPlacementValue"), value: z.string() }),
+            z.object({ TAG: z.literal("ImageNotFound"), url: z.string() }),
+        ]),
+    }),
+    z.object({
+        TAG: z.literal("VideoConversionError"),
+        error: z.union([
+            z.object({ TAG: z.literal("InvalidYouTubeUrl"), url: z.string() }),
+            z.object({ TAG: z.literal("UnexpectedYouTubeUrl"), url: z.string() }),
+            z.literal("YouTubeUrlContainsInvalidChars"),
+        ]),
+    }),
+    z.object({
+        TAG: z.literal("GalleryError"),
+        error: z.union([
+            z.object({ TAG: z.literal("UnexpectedNode"), node: z.string() }),
+            z.object({ TAG: z.literal("MissingImage"), url: z.string() }),
+            z.literal("EmptyGallery"),
+        ]),
+    }),
+])
+export type ContentParsingError = z.infer<typeof ContentParsingError>
+
+export const InvalidContentError = z.object({
+    TAG: z.literal("InvalidContent"),
+    error: z.object({
+        TAG: z.literal("ParsingErrors"),
+        errors: z.array(ContentParsingError),
+    }),
+})
+export type InvalidContentError = z.infer<typeof InvalidContentError>
--- src/clients/requests/publish-post.ts
+++ src/clients/requests/publish-post.ts
@@ -3,6 +3,7 @@
 import { z } from "zod"
 
 import * as Api from "src/clients/api"
+import * as Errors from "src/clients/errors"
 import * as Site from "src/models/site"
 import * as Post from "src/models/post"
 import * as Content from "src/models/content"
@@ -49,144 +50,7 @@ const Error = z.union([
                 TAG: z.literal("InvalidSlug"),
                 error: z.union([z.literal("NonAscii"), z.literal("UnsafeChars")]),
             }),
-            z.object({
-                TAG: z.literal("InvalidContent"),
-                error: z.object({
-                    TAG: z.literal("ParsingErrors"),
-                    errors: z.array(
-                        z.discriminatedUnion("TAG", [
-                            z.object({
-                                TAG: z.literal("UnimplementedNode"),
-                                node: z.string(),
-                            }),
-                            z.object({
-                                TAG: z.literal("UnexpectedNode"),
-                                node: z.string(),
-                            }),
-                            z.object({
-                                TAG: z.literal("HeadingNodeConversionError"),
-                                error: z.object({ TAG: z.literal("TooDeep"), level: z.number() }),
-                            }),
-                            z.object({
-                                TAG: z.literal("ListNodeConversionError"),
-                                error: z.union([
-                                    z.object({ TAG: z.literal("UnimplementedNode"), node: z.string() }),
-                                    z.object({ TAG: z.literal("UnexpectedNode"), node: z.string() }),
-                                    z.object({
-                                        TAG: z.literal("SubListConversionError"),
-                                        error: z.array(
-                                            z.union([
-                                                z.object({ TAG: z.literal("UnimplementedNode"), node: z.string() }),
-                                                z.object({ TAG: z.literal("UnexpectedNode"), node: z.string() }),
-                                                z.object({ TAG: z.literal("SubListConversionError"), error: z.any() }), // TODO: Handle recursive type
-                                            ]),
-                                        ),
-                                    }),
-                                ]),
-                            }),
-                            z.object({
-                                TAG: z.literal("BlockquoteNodeConversionError"),
-                                error: z.discriminatedUnion("TAG", [
-                                    z.object({ TAG: z.literal("UnimplementedNode"), node: z.string() }),
-                                    z.object({ TAG: z.literal("UnexpectedNode"), node: z.string() }),
-                                ]),
-                            }),
-                            z.object({
-                                TAG: z.literal("CalloutError"),
-                                error: z.union([
-                                    z.literal("InvalidSyntax"),
-                                    z.object({ TAG: z.literal("UnexpectedCalloutVariant"), variant: z.string() }),
-                                    z.object({
-                                        TAG: z.literal("BlockParsingError"),
-                                        error: z.discriminatedUnion("TAG", [
-                                            z.object({ TAG: z.literal("UnsupportedNode"), node: z.string() }),
-                                            z.object({
-                                                TAG: z.literal("HeadingError"),
-                                                error: z.object({ TAG: z.literal("TooDeep"), level: z.number() }),
-                                            }),
-                                            z.object({
-                                                TAG: z.literal("ListError"),
-                                                error: z.union([
-                                                    z.object({ TAG: z.literal("UnimplementedNode"), node: z.string() }),
-                                                    z.object({ TAG: z.literal("UnexpectedNode"), node: z.string() }),
-                                                    z.object({
-                                                        TAG: z.literal("SubListConversionError"),
-                                                        error: z.any(),
-                                                    }), // TODO: Handle recursive type
-                                                ]),
-                                            }),
-                                            z.object({
-                                                TAG: z.literal("QuoteError"),
-                                                error: z.discriminatedUnion("TAG", [
-                                                    z.object({ TAG: z.literal("UnimplementedNode"), node: z.string() }),
-                                                    z.object({ TAG: z.literal("UnexpectedNode"), node: z.string() }),
-                                                ]),
-                                            }),
-                                            z.object({
-                                                TAG: z.literal("ImageError"),
-                                                error: z.discriminatedUnion("TAG", [
-                                                    z.object({
-                                                        TAG: z.literal("UnexpectedParams"),
-                                                        params: z.string(),
-                                                    }),
-                                                    z.object({
-                                                        TAG: z.literal("UnexpectedPlacementValue"),
-                                                        value: z.string(),
-                                                    }),
-                                                    z.object({ TAG: z.literal("ImageNotFound"), url: z.string() }),
-                                                ]),
-                                            }),
-                                            z.object({
-                                                TAG: z.literal("VideoError"),
-                                                error: z.union([
-                                                    z.object({ TAG: z.literal("InvalidYouTubeUrl"), url: z.string() }),
-                                                    z.object({
-                                                        TAG: z.literal("UnexpectedYouTubeUrl"),
-                                                        url: z.string(),
-                                                    }),
-                                                    z.literal("YouTubeUrlContainsInvalidChars"),
-                                                ]),
-                                            }),
-                                            z.object({
-                                                TAG: z.literal("GalleryError"),
-                                                error: z.union([
-                                                    z.object({ TAG: z.literal("UnexpectedNode"), node: z.string() }),
-                                                    z.object({ TAG: z.literal("MissingImage"), url: z.string() }),
-                                                    z.literal("EmptyGallery"),
-                                                ]),
-                                            }),
-                                        ]),
-                                    }),
-                                ]),
-                            }),
-                            z.object({
-                                TAG: z.literal("ImageConversionError"),
-                                error: z.discriminatedUnion("TAG", [
-                                    z.object({ TAG: z.literal("UnexpectedParams"), params: z.string() }),
-                                    z.object({ TAG: z.literal("UnexpectedPlacementValue"), value: z.string() }),
-                                    z.object({ TAG: z.literal("ImageNotFound"), url: z.string() }),
-                                ]),
-                            }),
-                            z.object({
-                                TAG: z.literal("VideoConversionError"),
-                                error: z.union([
-                                    z.object({ TAG: z.literal("InvalidYouTubeUrl"), url: z.string() }),
-                                    z.object({ TAG: z.literal("UnexpectedYouTubeUrl"), url: z.string() }),
-                                    z.literal("YouTubeUrlContainsInvalidChars"),
-                                ]),
-                            }),
-                            z.object({
-                                TAG: z.literal("GalleryError"),
-                                error: z.union([
-                                    z.object({ TAG: z.literal("UnexpectedNode"), node: z.string() }),
-                                    z.object({ TAG: z.literal("MissingImage"), url: z.string() }),
-                                    z.literal("EmptyGallery"),
-                                ]),
-                            }),
-                        ]),
-                    ),
-                }),
-            }),
+            Errors.InvalidContentError,
         ]),
     }),
     z.object({
